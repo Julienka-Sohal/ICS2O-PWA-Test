1aedde4fc7f483105b882424f051c00be2d4de93
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2022-11-15 19:15:03 +0000

    if statement update

--- js/script.js
+++ js/script.js
@@ -10,14 +10,26 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-01-JS/sw.js", {
+    scope: "/ICS2O-Unit5-01-JS/",
   })
 }
 
+// returns a random integer from 1 to 6 into variable "randomNumber"  
+const randomNumber = Math.floor(Math.random() * 6) + 1  
+
 /**
- * This function displays an alert.
+ * This function updates slader value
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function updatesSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+
+  //process
+  if (valueFromSlider == randomNumber) {
+    document.getElementById("answer").innerHTML = "the answer was, " + randomNumber + "!" + " You got it! good job"
+  }
+
+  if (valueFromSlider != randomNumber) {
+    document.getElementById("answer").innerHTML = "the answer was, " + randomNumber + "!" + " Nice guess, but try again"
+  }
 }

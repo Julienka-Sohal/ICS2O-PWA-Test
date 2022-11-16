81f8076ca9584fd6d5ccbbb95998a07895286c94
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2022-11-16 19:21:33 +0000

    program update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ICS2O-PWA-Template-HTML
 
-[![Mr Coxall's Super Linter](https://github.com/Julienka-Sohal/ICS2O-Unit5-01-JS/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/Julienka-Sohal/ICS2O-Unit5-01-JS/actions/)
+[![Mr Coxall's Super Linter](https://github.com/Julienka-Sohal/ICS2O-Unit5-02-JS/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/Julienka-Sohal/ICS2O-Unit5-02-JS/actions/)
 
-[![Run on Repl.it](https://repl.it/badge/github/Julienka-Sohal/ICS2O-Unit5-01-JS)](https://repl.it/github/Julienka-Sohal/ICS2O-Unit5-01-JS)
+[![Run on Repl.it](https://repl.it/badge/github/Julienka-Sohal/ICS2O-Unit5-02-JS)](https://repl.it/github/Julienka-Sohal/ICS2O-Unit5-02-JS)
 
-This site can be found at: [https://Julienka-Sohal.github.io/ICS2O-Unit5-01-JS/](https://Julienka-Sohal.github.io/ICS2O-Unit5-01-JS/)
+This site can be found at: [https://Julienka-Sohal.github.io/ICS2O-Unit5-02-JS/](https://Julienka-Sohal.github.io/ICS2O-Unit5-02-JS/)
--- js/script.js
+++ js/script.js
@@ -10,28 +10,22 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-01-JS/sw.js", {
-    scope: "/ICS2O-Unit5-01-JS/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-02-JS/sw.js", {
+    scope: "/ICS2O-Unit5-02-JS/",
   })
 }
 
 // returns a random integer from 1 to 6 into variable "randomNumber"
 const randomNumber = Math.floor(Math.random() * 6) + 1
 
-/**
- * This function updates slader value
- */
-function updatesSliderValue(valueFromSlider) {
-  document.getElementById("slider-value").innerHTML = valueFromSlider
-
   //process
-  if (valueFromSlider == randomNumber) {
-    document.getElementById("answer").innerHTML =
-      "the answer was, " + randomNumber + "!" + " You got it! good job"
-  }
+  function myButtonClicked() {
+  buttonOnChecked = document.getElementById('roll1').checked
 
-  if (valueFromSlider != randomNumber) {
-    document.getElementById("answer").innerHTML =
-      "the answer was, " + randomNumber + "!" + " Nice guess, but try again"
+  if (buttonOnChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+    "You rolled for " + randomNumber + "."
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+    "You rolled for -" + randomNumber + "."
   }
-}

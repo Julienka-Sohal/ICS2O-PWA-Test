fb27cc75d513f0e417c042d59079afe1ca7b5807
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2022-11-17 18:50:39 +0000

    comments, title and meta tags

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Julienka Sohal All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julienka Sohal
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"

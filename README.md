51a0bf6ca23b32d078e62bb937f7ffc3a71619e2
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2022-11-24 21:01:44 +0000

    program update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# ICS2O-Unit5-03-JS
+# ICS2O-Unit5-04-JS
 
-[![Mr Coxall's Super Linter](https://github.com/Julienka-Sohal/ICS2O-Unit5-03-JS/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/Julienka-Sohal/ICS2O-Unit5-03-JS/actions/)
+[![Mr Coxall's Super Linter](https://github.com/Julienka-Sohal/ICS2O-Unit5-04-JS/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/Julienka-Sohal/ICS2O-Unit5-04-JS/actions/)
 
-[![Run on Repl.it](https://repl.it/badge/github/Julienka-Sohal/ICS2O-Unit5-03-JS)](https://repl.it/github/Julienka-Sohal/ICS2O-Unit5-03-JS)
+[![Run on Repl.it](https://repl.it/badge/github/Julienka-Sohal/ICS2O-Unit5-04-JS)](https://repl.it/github/Julienka-Sohal/ICS2O-Unit5-04-JS)
 
-This site can be found at: [https://Julienka-Sohal.github.io/ICS2O-Unit5-03-JS/](https://Julienka-Sohal.github.io/ICS2O-Unit5-03-JS/)
+This site can be found at: [https://Julienka-Sohal.github.io/ICS2O-Unit5-04-JS/](https://Julienka-Sohal.github.io/ICS2O-Unit5-04-JS/)
--- js/script.js
+++ js/script.js
@@ -10,26 +10,21 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-03-JS/sw.js", {
-    scope: "/ICS2O-Unit5-03-JS/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-04-JS/sw.js", {
+    scope: "/ICS2O-Unit5-04-JS/",
   })
 }
 
 //process
 function myButtonClicked() {
-  const buttonOnChecked = document.getElementById("ageInput").value
-
-  if (buttonOnChecked >= 17) {
-    document.getElementById("movie-maturity-recommendation").innerHTML =
-      "You are old enough to watch an R rated movie, PG-14 movies and G rated movies if you wish!"
-  } else if (buttonOnChecked >= 13) {
-    document.getElementById("movie-maturity-recommendation").innerHTML =
-      "You are old enough to watch a PG-13 movie! However not an R rated movie yet..."
-  } else if (buttonOnChecked >= 4) {
-    document.getElementById("movie-maturity-recommendation").innerHTML =
-      "You are old enough to watch a G rated movie! However not an PG-13 movies or R rated movies..."
+  const day = document.getElementById("week").value
+  const typeAge = document.getElementById("ageInput").value
+  
+  if ((day == "Tuesday" || day == "Thursday") || (typeAge >= 12 && typeAge <= 25)) {
+    document.getElementById("weekday-discount").innerHTML = 
+      "You are eligible to get a discount!"
   } else {
-    document.getElementById("movie-maturity-recommendation").innerHTML =
-      "You shouldn't be watching movies at all!"
+    document.getElementById("weekday-discount").innerHTML =
+      "You are not eligible to get a discount..."
   }
 }
